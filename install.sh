6344a6a409013791ced7aa7ce2c4a3979050bc66
Aladin Bouzerd <aladin.bouzerd@gmail.com>
2017-09-25 00:15:57 +0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,26 @@
+######### Docker
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce;
+
+######### Docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
+sudo chmod +x /usr/local/bin/docker-compose
+
+############ Git
+sudo apt-get install -y git
---
+++ readme.md
@@ -0,0 +1,12 @@
+Getting Set Up
+==========
+
+Create a new server, then copy the contents of `install.sh` and run them inside the server.
+
+Once the installation is done, clone this repository and start:
+
+```
+git clone git://github.com/magictutorials/webapp.git
+cd webapp
+docker-compose up -d
+```
